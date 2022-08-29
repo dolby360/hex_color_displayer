3ed752620b7e8c4b1a368fa669a3e3c6191a16a1
TomDamri <tomdamri@gmail.com>
2022-08-29 19:33:39 +0300

    added - React Testing Library; fixed - testing system

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import renderer from 'react-test-renderer'
 import HexColorDisplay from '../src/index'
+import {render} from '@testing-library/react'
 
 // ====================================
 // Test Fixtures
@@ -21,11 +21,10 @@ const raw = [54,22,3,6,23,6,1,2,3,4,5,6,7,8,9,10,11,12,13];
 // ====================================
 
 describe('Test HexColorDisplay sanpshot', () =>{
-    it('Matches the snapshot', async () => {
-        const component = await renderer.create(<HexColorDisplay offsets={data_example} bin={raw} />);
-        //const tree = await component.toJSON();
-        //await expect(tree).toMatchSnapshot();
-        expect(1).toBe(1)
+    it('Matches the snapshot',  () => {
+        const component = render(<HexColorDisplay offsets={data_example} bin={raw} />);
+        const tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
     })
 })
 
