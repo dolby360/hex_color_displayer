43915118b7e771608db034a111da007a5e54a012
Elad Motzny <emotzny@gmail.com>
2022-09-24 12:32:32 +0300

    changed to every

--- src/index.js
+++ src/index.js
@@ -69,12 +69,14 @@ class HexColorDisplay extends React.Component {
         this.hexItemsList = Array.from(this.saveHexItemsList);
         this.asciiItemsList = Array.from(this.saveAsciiItemsList);
         var i = index;
-        this.hexItemsList.every(item => {
-            if (item.props.gId !== gid){
+        this.hexItemsList.slice(0, index).reverse().every(item => {
+             if(item.props.gId !== gid){
                 return false;
-            }
-            return true;
+             }
+             i--;
+             return true;
         });
+
         let startIndexToUpdate = i; // to use later when updating rowState
         let groupFound = false;
         let style = 'item_disabled'
@@ -104,7 +106,7 @@ class HexColorDisplay extends React.Component {
         let rowNumber = Math.floor(startIndexToUpdate / 16);
         let rowState = this.state.rows;
         let resoluton = 100;
-        for (let i = 0; i < resoluton; i++) {//make a map function and use it on hexArray and asciiArray
+        for (let i = 0; i < resoluton; i++) {
             let rowNumberToUpdate = ((rowNumber <= (resoluton / 2))
                 ? i : (rowNumber - ((resoluton / 2)) + i));
             let startIndex = (rowNumberToUpdate * 16);
@@ -123,10 +125,10 @@ class HexColorDisplay extends React.Component {
 
     splitItemsList = () => {
         let arr = []
-        for (let i = 0; i < this.hexItemsList.length; i += 16) {//for each
+        for (let i = 0; i < this.hexItemsList.length; i += 16) {
             let hexArray = this.hexItemsList.slice(i, i + 16)
             let asciiArray = this.asciiItemsList.slice(i, i + 16)
-            for (let j = hexArray.length; j < 16; j++) {//for each
+            for (let j = hexArray.length; j < 16; j++) {
                 let item = this.getItem(
                     'item_disabled', -1, -1, '.', '', asciiArray.length, ''
                 )
@@ -183,25 +185,6 @@ class HexColorDisplay extends React.Component {
     // buffer is the binary buffer.
     setItems = (data, buffer) => {
         let gid = 0;
-        // for (let i = 0; i < data.length; i++) {//for each
-        //     let startIndex = data[i].start;
-        //     if (data[i].sublist.length > 0) {
-        //         this.setChunk(
-        //             buffer.slice(data[i].start, data[i].sublist[0].start),
-        //             data[i].name, startIndex, gid);
-        //         for (let k = 0; k < data[i].sublist.length; k++) {//for each
-        //             this.setChunk(
-        //                 buffer.slice(data[i].sublist[k].start, data[i].sublist[k].end),
-        //                 data[i].sublist[k].name, startIndex, gid);
-        //             startIndex += data[i].sublist[k].end;
-        //         }
-        //     } else {
-        //         this.setChunk(
-        //             buffer.slice(data[i].start, data[i].end),
-        //             data[i].name, startIndex, gid);
-        //     }
-        //     gid++;
-        // }
         data.forEach(item => {
             let startIndex = item.start;
             if (item.sublist.length > 0) {
@@ -231,10 +214,7 @@ class HexColorDisplay extends React.Component {
         var buffer = [];
         var bytes = this.props.bin.length;
         var i = 0;
-        
-        // for (i = 0; i < bytes; i++) {//forEach?
-        //     buffer.push(this.props.bin[i]);
-        // }
+
         this.props.bin.forEach(item => {
             buffer.push(item)
         })
